a00ed0eb6e8e5d11aab3c0c42ef727f64d1c452a
crams <cpramsey112@gmail.com>
2023-05-25 10:09:46 -0400

    inquirer prompts written

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 const inquirer = require('inquirer');
 const { SvgBuilder } = require('svg-builder');
 const fs = require('fs');
+const { ChildProcess } = require('child_process');
 
 const shapeOptions = ['circle', 'triangle', 'square'];
+
+inquirer
+    ,prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three charactetrs for your logo',
+            validate: (input) => {
+                return input.length <= 3 ? true : ''
+            }, 
+        }, 
+        {
+            type: 'input', 
+            name: 'text', 
+            message: 'Enter the text color (color keyword or hexadecimal number):',
+        },
+
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape for the logo'
+            choices: shapeOptions, 
+
+        },
+
+        {
+            type: 'input'
+            name: 'shapeColor'
+            message: 'Enter the shaope color (keyword or hex):', 
+        },
+    ])
+        
