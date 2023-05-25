b07a9897ed9e248e14dee404629ef03cf0f5d921
crams <cpramsey112@gmail.com>
2023-05-25 10:53:17 -0400

    the condition written

--- index.js
+++ index.js
@@ -35,4 +35,38 @@ inquirer
             message: 'Enter the shaope color (keyword or hex):', 
         },
     ])
+    .then((answers) => {
+        //process user input for file gen
+
+        const { text, textColor, shape, shapeColor } = answers;
+        const svg = new SvgBuilder();
+
+        // set props
+        svg.width(300).height(200).viewbox(0,0,300,200);
+
+        //create shape based on user input
+        switch (shape) {
+            case 'circle':
+                svg.circle(150,100,50).fill(shapeColor);
+                break;
+            case 'triangle':
+                svg.polygon('150,20 250, 180 50, 180').fill(shapeColor);
+                break;
+            case 'square':
+                svg.rect(100,50,100,100).fill(shapeColor);
+                break;
+        }
+
+        //add text
+        svg.text(150, 125, text).fill(textColor).fontFamily('Arial').fontSize(40).anchor('middle');
+
+        //save to a file
+        fs.writeFileSync('logo.svg', svg.toString());
+
+        console.log('Generated logo.svg');
+
+    })
+    .catch((error) => {
+        console.error(error);
+    });
         
